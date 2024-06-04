9290193c7072a9f738cf3846e89ba65346d89c1a
RenatoAF <renato012br@hotmail.com>
2024-06-04 18:03:55 -0300

    commit 3

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
-
+app.get('/', (request, response) => {
+    return
+    response.status(200)
+    .json({
+        mensage:"Hello"
+    });
+});
 
 module.exports = app;
